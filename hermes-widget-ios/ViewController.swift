9604ba3ef812de6390e793d172c3ac0e8df18d70
Bezaleel Ashefor <knightbenax@gmail.com>
2023-01-15 19:25:26 +0100

    Initial Commit

---
+++ hermes-widget-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hermes-widget-ios
+//
+//  Created by Bezaleel Ashefor on 15/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
