b8999e3271b9165fb389e2537a303c68ddbf3a47
Bezaleel Ashefor <3107111+knightbenax@users.noreply.github.com>
2024-05-29 18:23:25 +0100

    Change Full Structure of Things

---
+++ Diesel/Diesel/Diesel.h
@@ -0,0 +1,18 @@
+//
+//  Diesel.h
+//  Diesel
+//
+//  Created by Bezaleel Ashefor on 29/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Diesel.
+FOUNDATION_EXPORT double DieselVersionNumber;
+
+//! Project version string for Diesel.
+FOUNDATION_EXPORT const unsigned char DieselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Diesel/PublicHeader.h>
+
+
---
+++ Diesel/DieselExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DieselExample
+//
+//  Created by Bezaleel Ashefor on 29/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
